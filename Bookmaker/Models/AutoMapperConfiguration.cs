056a9f4ca31775abed624eef853bbef36d05fa2c
michelc <michelc@gmail.com>
2012-08-25 11:46:04 +0200

    Simplification AutoMapping des enums

--- Bookmaker/Models/AutoMapperConfiguration.cs
+++ Bookmaker/Models/AutoMapperConfiguration.cs
@@ -9,12 +9,12 @@ namespace Bookmaker.Models
             Mapper.CreateMap<Booklet, JsonBooklet>();
 
             Mapper.CreateMap<Travel, JsonTravel>()
-                .ForMember(dest => dest.TravelType, opt => opt.MapFrom(src => src.TypeTravel.ToString()));
+                .ForMember(dest => dest.TravelType, opt => opt.MapFrom(src => src.TypeTravel));
 
             Mapper.CreateMap<Price, JsonPrice>();
 
             Mapper.CreateMap<Section, JsonSection>()
-                .ForMember(dest => dest.SectionType, opt => opt.MapFrom(src => src.TypeSection.ToString()));
+                .ForMember(dest => dest.SectionType, opt => opt.MapFrom(src => src.TypeSection));
         }
     }
 }
