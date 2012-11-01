83de298cf37754f9b985fee8a8ea927fd5b75e95
michelc <michelc@gmail.com>
2012-11-01 10:05:48 +0100

    Activation des erreurs personnalisées
    (pour éviter l'écran jaune de la mort affichant trop d'informations
    sensibles)

--- Bookmaker/Controllers/HomeController.cs
+++ Bookmaker/Controllers/HomeController.cs
@@ -33,7 +33,9 @@ namespace Bookmaker.Controllers
 
         public ActionResult About()
         {
-            return View();
+            // return View();
+
+            throw new System.Exception("Quelquefois une erreur survient !");
         }
     }
 }
