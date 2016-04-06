063309a6adf4e2c9ce8b02eb4f069c4ba9146402
Michel <michelc@gmail.com>
2016-04-06 12:42:44 +0200

    LowercaseRoutesMVC n'est plus nécessaire.

--- Bookmaker/App_Start/RouteConfig.cs
+++ Bookmaker/App_Start/RouteConfig.cs
@@ -1,6 +1,5 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
-using LowercaseRoutesMVC;
 
 namespace Bookmaker
 {
@@ -9,8 +8,9 @@ namespace Bookmaker
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.LowercaseUrls = true;
 
-            routes.MapRouteLowercase(
+            routes.MapRoute(
                 name: "Default",
                 url: "{root_id}/{controller}/{action}/{id}",
                 defaults: new { root_id = "0", controller = "Home", action = "Index", id = UrlParameter.Optional }
